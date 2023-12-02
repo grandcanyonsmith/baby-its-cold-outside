41b0c2f98353c22bff2f8ff0f8f316028bebaa9f
Canyon Smith <canyonfsmith@gmail.com>
2023-12-02 16:35:51 -0700

    Updated main.py!

--- main.py
+++ main.py
@@ -1,18 +1,4 @@
-from datetime import datetime
-import pytz
-
-def print_utah_time():
-    # Timezone for Utah - Either 'America/Denver' can be used for Mountain Time
-    utah_timezone = pytz.timezone('America/Denver')
+def say_hello_world():
+    print("Hello World")
     
-    # Current time in UTC
-    utc_time = datetime.utcnow()
-    
-    # Convert to Utah time
-    utah_time = utc_time.astimezone(utah_timezone)
-    
-    # Print the time in Utah
-    print("Current time in Utah:", utah_time.strftime('%Y-%m-%d %H:%M:%S %Z%z'))
-
-if __name__ == '__main__':
-    print_utah_time()
+say_hello_world()
